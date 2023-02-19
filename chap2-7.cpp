f4640b70e273030c0ac45b49aae899c0890cb9cf
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-19 14:32:51 -0500

    Create chap2-7.cpp

---
+++ chap2-7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+	using namespace std;
+	int main ()
+	{
+		cout << "Two mandolins like creatures in the \n \n" ;
+		cout << "dark \n \n";
+		cout << "Creating the agony of ecstasy. \n \n" ;
+		cout << "				- George Barker \n \n";
+		return 0;
+
+}

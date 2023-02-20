86225658252f2bfcb898272982a186510a9e951a
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-20 04:24:31 -0500

    Create chapter3-1.cpp

---
+++ chapter3-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main ()
+{         int age;
+          double pay;
+          char section;
+
+cin >> age >> pay >> section ;
+
+return 0 ;
+
+  }

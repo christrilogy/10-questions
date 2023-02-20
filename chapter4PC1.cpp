f2862ee7f6704d42c54f4b940ced817f534b9fae
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-20 04:56:37 -0500

    Create chapter4PC1.cpp

---
+++ chapter4PC1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{    
+     double num1, num2;
+
+    cout << "Enter two number" << endl;
+    cin >> num1 >> num2;
+
+    if (num1 > num2) 
+      cout << num1 << " is greater than " << num2 << endl;
+    else 
+      cout << num2 << " is greater than " << num1 << endl;
+
+return 0;
+
+}
